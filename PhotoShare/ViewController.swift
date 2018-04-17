a1b2082e8be5ea787af10a52ff7d0134583f2f84
Carmine Massei <carminemassei@macbook-pro-di-carmine.local>
2018-04-17 10:02:44 +0200

    Initial Commit

---
+++ PhotoShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoShare
+//
+//  Created by Carmine Massei on 17/04/18.
+//  Copyright © 2018 iOSFoundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
